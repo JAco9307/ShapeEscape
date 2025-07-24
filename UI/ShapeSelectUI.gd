0ab577ea014cc35c569ee5b82d8861bb1f14cfeb
Jaco9307 <skrivtiljacob@hotmail.com>
2025-07-24 15:59:26 +0200

    Menu format changes

--- UI/ShapeSelectUI.gd
+++ UI/ShapeSelectUI.gd
@@ -7,24 +7,70 @@ var shapes = [
 		"object": preload("res://models/horse.glb"),
 		"name": "horse",
 		"owned": -1,
+		"rarity": 0,
 		"scene": preload("res://objects/horse.tscn")
 	},
 	{
 		"object": preload("res://models/shark.glb"),
 		"name": "shark",
-		"owned": 0,
+		"owned": 1,
+		"rarity": 1,
 		"scene": preload("res://objects/shark.tscn")
 	},
 	{
 		"object": preload("res://models/shark.glb"),
 		"name": "shark2",
-		"owned": 0,
+		"owned": 1,
+		"rarity": 1,
 		"scene": preload("res://objects/shark.tscn")
 	},
 	{
 		"object": preload("res://models/shark.glb"),
 		"name": "shark3",
 		"owned": 2,
+		"rarity": 1,
+		"scene": preload("res://objects/shark.tscn")
+	},
+	{
+		"object": preload("res://models/shark.glb"),
+		"name": "shark4",
+		"owned": 2,
+		"rarity": 1,
+		"scene": preload("res://objects/shark.tscn")
+	},
+	{
+		"object": preload("res://models/shark.glb"),
+		"name": "shark5",
+		"owned": 2,
+		"rarity": 1,
+		"scene": preload("res://objects/shark.tscn")
+	},
+	{
+		"object": preload("res://models/shark.glb"),
+		"name": "shark6",
+		"owned": 2,
+		"rarity": 1,
+		"scene": preload("res://objects/shark.tscn")
+	},
+	{
+		"object": preload("res://models/shark.glb"),
+		"name": "shark7",
+		"owned": 2,
+		"rarity": 1,
+		"scene": preload("res://objects/shark.tscn")
+	},
+	{
+		"object": preload("res://models/shark.glb"),
+		"name": "shark8",
+		"owned": 2,
+		"rarity": 1,
+		"scene": preload("res://objects/shark.tscn")
+	},
+	{
+		"object": preload("res://models/shark.glb"),
+		"name": "shark9",
+		"owned": 2,
+		"rarity": 1,
 		"scene": preload("res://objects/shark.tscn")
 	},
 ]
@@ -57,14 +103,19 @@ func setup_buttons():
 		var newButton = shapeButtonScene.instantiate()
 		var buttonObject = shape.object.instantiate()
 		%ShapeButtons.add_child(newButton)
+		buttonObject.scale = Vector3(1,1,1) * 0.8
+		
 		newButton.set_location_offset(offset)
 		offset += Vector3(0, 10000, 10000)
+		
 		newButton.change_object(buttonObject)
+		newButton.custom_minimum_size = Vector2(150,150)
+
 		if shape.owned == -1:
 			newButton.set_info("Default")
 		else:
 			newButton.set_info(shape.name + " - " + str(shape.owned) + " owned")
-			
+		
 		newButton.pressed.connect(button_clicked.bind(index))
 		index += 1
 		
