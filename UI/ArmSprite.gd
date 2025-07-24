9170acf1b434c427c4d8665510290c79a8b610b0
Jaco9307 <skrivtiljacob@hotmail.com>
2025-07-24 02:49:19 +0200

    hand rotates and stuff

--- UI/ArmSprite.gd
+++ UI/ArmSprite.gd
@@ -1,23 +1,22 @@
 extends AnimatedSprite2D
 
 var camera:Camera3D 
-
+var axis = Vector3(0,1,0)
 
 func _ready():
 	camera = get_viewport().get_camera_3d()
 
 func _process(delta: float) -> void:
-	if GlobalVariables.selectedObject == null:
+	if GlobalVariables.selectedObject == null: #free hand
 		global_position = get_global_mouse_position()
 		frame = 0
 		rotation_degrees = 0
-	else:
+	else: 	#grabbing something hand
 		global_position = camera.unproject_position(GlobalVariables.selectedObject.global_position)
 		frame = 1
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
-	$Label2.text = str(GlobalVariables.handrot.y)
-	$Label3.text = str(GlobalVariables.handrot.z)
+		
+		rotation_degrees = -GlobalVariables.selectedObject.rotation_degrees.y
 		
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
