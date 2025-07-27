7dcadbd98ac5971cf870d15b5722aeea6b43fd54
Jaco9307 <skrivtiljacob@hotmail.com>
2025-07-27 22:32:36 +0200

    Wrist break sound

--- UI/Hand/pin_joint_2d.gd
+++ UI/Hand/pin_joint_2d.gd
@@ -4,7 +4,22 @@ extends PinJoint2D
 @export var limit_gw: int = 45 #limit in glockwise direction
 @export var control_node: Node2D  #literally Node_B
 
+var rng = RandomNumberGenerator.new()
 var lastangle = 0
+
+var sound = [
+	preload("res://music/Sounds/Wrist/1.mp3"),
+	preload("res://music/Sounds/Wrist/2.mp3"),
+	preload("res://music/Sounds/Wrist/3.mp3"),
+	preload("res://music/Sounds/Wrist/4.mp3"),
+	preload("res://music/Sounds/Wrist/5.mp3"),
+	preload("res://music/Sounds/Wrist/6.mp3"),
+	preload("res://music/Sounds/Wrist/7.mp3"),
+	preload("res://music/Sounds/Wrist/8.mp3"),
+	preload("res://music/Sounds/Wrist/9.mp3"),
+	preload("res://music/Sounds/Wrist/10.mp3"),
+	preload("res://music/Sounds/Wrist/11.mp3")
+]
 
 func _process(_delta):	
 	# motor
@@ -20,5 +35,7 @@ func _process(_delta):
 		
 	var sum = lastangle - control_node.rotation_degrees
 	if sum > 200 or sum < -200:
+		var roll = rng.randi_range(10,0)
+		$AudioStreamPlayer.stream = sound[roll]
 		$AudioStreamPlayer.play()
 	lastangle = control_node.rotation_degrees
