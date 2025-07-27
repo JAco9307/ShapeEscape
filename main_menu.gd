acc147ff6c2afb38076a4aa21c778c886287e846
Jaco9307 <skrivtiljacob@hotmail.com>
2025-07-27 09:53:33 +0200

    idkkkk

--- main_menu.gd
+++ main_menu.gd
@@ -4,10 +4,8 @@ extends Node2D
 func _on_start_button_pressed() -> void:
 	print("start")
 
-
 func _on_level_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main.tscn")
-
 
 func _on_credits_button_pressed() -> void:
 	$Credits.visible = true
